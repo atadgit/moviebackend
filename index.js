7655c3da3df8cc3d96ae839930813912aa051acb
atadgit <tadbhavdagr8@gmail.com>
2024-05-18 23:27:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+export default app
+
+import dotenv from 'dotenv'
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient = mongodb.MongoClient
+dotenv.config();
+
+const port = 8000
+
+MongoClient.connect(
+  process.env.uri,
+  {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true
+  })
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    await ReviewsDAO.injectDB(client)
+    app.listen(port, () => {
+      console.log(`listening`)
+    })
+  })
